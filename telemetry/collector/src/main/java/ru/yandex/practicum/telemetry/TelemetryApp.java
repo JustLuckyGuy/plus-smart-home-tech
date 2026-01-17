86f03f49e90bf20c90aab98349988ad2050a261a
Tolykov Kirill <kirillmax@mail.ru>
2026-01-18 01:23:29 +0300

    Исправление ошибок

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/TelemetryApp.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/TelemetryApp.java
@@ -3,9 +3,9 @@ package ru.yandex.practicum.telemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "ru.yandex.practicum.telemetry.kafka_producer," +
-        "ru.yandex.practicum.telemetry.collector," +
-        "main.avro.serializator")
+@SpringBootApplication(scanBasePackages = "main.avro.serializator," +
+        "ru.yandex.practicum.telemetry.kafka_producer," +
+        "ru.yandex.practicum.telemetry.collector,")
 public class TelemetryApp {
     public static void main(String[] args) {
         SpringApplication.run(TelemetryApp.class, args);
